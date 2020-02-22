0569aecd7ceb5eb60835fe19f21a2449434e19c4
Juan Pablo Salamanca Ramírez <jpsalamarcara@unal.edu.co>
2020-02-22 16:24:43 -0500

    Docs about point b are ready

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 
 ## Question A
+Your goal for this question is to write a program that accepts two lines (x1,x2) and (x3,x4) on the x-axis and returns whether they overlap. As an example, (1,5) and (2,6) overlaps but not (1,5) and (6,8).
 
 [Solution](/juan/point_a.py)
 
@@ -19,9 +20,36 @@ print(output)
 
 ```
 
+#### Developer Observations
+1. For every input vector `a` or `b`, it does not matter if `a[0]` is greater or less than `a[1]`
+
 
 ## Question B
+The goal of this question is to write a software library that accepts 2 version string as input and returns whether one is greater than, equal, or less than the other. As an example: “1.2” is greater than “1.1”. Please provide all test cases you could think of.
+
 [Solution](/juan/point_b.py)
+
+[Tests](/test/tests_for_point_b.py)
+
+
+#### Example
+
+```python3
+
+from juan.point_b import check_version
+v1 = '10.1.3.0.0.0'
+v2 = '10.1.3'
+output = check_version(v1, v2)
+print(output)
+
+```
+
+
+#### Developer Observations
+`check_version` supports alpha and beta versions with literals `a` or `b` at the end of every version string.
+
+For example `'10.1.3a'` is a valid version code
+
 
 
 ## Question C
--- juan/point_b.py
+++ juan/point_b.py
@@ -6,13 +6,13 @@ assertion_message = '{} must have a pattern like: {}'
 literal_decoder = {'a': -0.2, 'b': -0.1}
 
 
-def decode_value(code):
+def decode_value(code: str) -> float:
     if re.match(alpha_beta_subversion_pattern, code):
-        value = int(code[:-1])
+        value = float(code[:-1])
         value = value + literal_decoder[code[-1]]
         return value
     else:
-        return int(code)
+        return float(code)
 
 
 def check_residual(version1: list, version2: list) -> tuple:
@@ -26,6 +26,10 @@ def check_residual(version1: list, version2: list) -> tuple:
 
 
 def check_version(version1: str, version2: str) -> tuple:
+    # returns an integer tuple: (0, 0) if versions are equal
+    #                          (1, -1) if version1 is greater than version2
+    #                          (-1, 1) if version2 is greater than version1
+    #
     assert re.match(version_pattern, version1), assertion_message.format('version1', version_pattern)
     assert re.match(version_pattern, version2), assertion_message.format('version2', version_pattern)
     version1 = version1.split('.')
@@ -37,6 +41,7 @@ def check_version(version1: str, version2: str) -> tuple:
             return tuple((1, -1))
         elif value2 > value1:
             return tuple((-1, 1))
+    # After check every matched version number pair, it checks if there is a residual version code greater than zero
     if len(version1) > len(version2):
         return check_residual(version1, version2)
     elif len(version2) > len(version1):
