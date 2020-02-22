0cf4df54dc78cc3dbca17fbea75def277a2d9f59
Juan Pablo Salamanca Ramírez <jpsalamarcara@unal.edu.co>
2020-02-22 15:51:54 -0500

    Description of point a

--- README.md
+++ README.md
@@ -4,6 +4,21 @@
 ## Question A
 [Solution](/juan/point_a.py)
 
+[Tests](/test/tests_for_point_a.py)
+
+### example
+
+´´´python3
+
+from juan.point_a import check_overlap
+a = (3, 8)
+b = (6, 11)
+output = check_overlap(a, b)
+print(output)
+
+´´´
+
+
 
 ## Question B
 [Solution](/juan/point_b.py)
--- juan/point_a.py
+++ juan/point_a.py
@@ -3,4 +3,5 @@
 def check_overlap(vector_a: tuple, vector_b: tuple) -> bool:
     assert len(vector_a) == len(vector_b), 'vectors must have the same rank'
     assert len(vector_a) == 2, 'only 2 rank vectors are allowed'
+    # It's cheaper check if there is not overlap (intersection or inception) than checking if there is
     return not (max(vector_a) < min(vector_b) or max(vector_b) < min(vector_a))
